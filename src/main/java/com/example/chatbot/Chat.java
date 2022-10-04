e6cb00ce9c0aa9b04660a095cf07c92b6f69832d
Bash-Code <bachartrv2000@gmail.com>
2022-10-04 09:26:11 +0100

    database

---
+++ src/main/java/com/example/chatbot/Chat.java
@@ -0,0 +1,12 @@
+package com.example.chatbot;
+
+public class Chat {
+    private String message;
+    private String reponse;
+
+    public Chat(String message, String reponse) {
+        this.message = message;
+        this.reponse = reponse;
+    }
+    
+}
