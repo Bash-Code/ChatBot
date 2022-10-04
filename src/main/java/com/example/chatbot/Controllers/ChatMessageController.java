f518e3e4ae31e291788349f03f49bbd8c10f1689
ouhaddaOualid <83510523+ouhaddaOualid@users.noreply.github.com>
2022-10-04 09:27:23 +0100

    creation of models, controllers and adding deppendencies

---
+++ src/main/java/com/example/chatbot/Controllers/ChatMessageController.java
@@ -0,0 +1,5 @@
+package com.example.chatbot.Controllers;
+
+public class ChatMessageController {
+
+}
---
+++ src/main/java/com/example/chatbot/Models/MessageType.java
@@ -0,0 +1,5 @@
+package com.example.chatbot.Models;
+
+public enum MessageType {
+    CHAT,RESPONSE,JOIN,LEAVE
+}
