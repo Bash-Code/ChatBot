0a6d726f551502dd67ebc40173f80c0f67451b0b
Bash-Code <bachartrv2000@gmail.com>
2022-11-07 21:07:35 +0100

    chatbot final3

--- src/main/java/com/example/chatbot/Langdetect/LanguageDetector.java
+++ src/main/java/com/example/chatbot/Langdetect/LanguageDetector.java
@@ -26,6 +26,10 @@ public class LanguageDetector {
 
         System.out.println("Predicted language: "+ languageMapper.getLanguage(languages[0].getLang()));
 
+        // uncomment to know confidence for rest of the languages
+	       /* for(Language language:languages){
+	            System.out.println(language.getLang()+"  confidence:"+language.getConfidence());
+	        }*/
         return languageMapper.getLanguage(languages[0].getLang());
     }
 }
